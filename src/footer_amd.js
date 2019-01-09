67b1b8c7e0ad17fae841a64048a868f8d38b3e0b
David Shams <david.shams@gmail.com>
2019-01-09 15:03:18 -0600

    RequireJS cannot go to nested folder by default require(['lib/jquery']

--- src/footer_amd.js
+++ src/footer_amd.js
@@ -1,4 +1,5 @@
 // require(['../lib/jquery'], function($){ /** when jQuery is loaded run this callback function */
+// require(['lib/jquery'], function($){ /** RequireJS cannot go to nester folder folder */
 require(['jquery'], function($){ /** RequireJS cannot go to parent folder */
     alert('AMD loaded with require.js');
     $('h3').css({border: 'solid 1px red'})
