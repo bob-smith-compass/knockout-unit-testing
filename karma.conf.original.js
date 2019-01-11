1528a5fcdf53514a6340802a54555aa52566402a
David Shams <david.shams@gmail.com>
2019-01-10 22:09:23 -0600

    wow! As always if you don't understand it it must be something simple, Knockout with Karma works

--- karma.conf.original.js
+++ karma.conf.original.js
@@ -90,26 +90,27 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './src',
+    basePath: '',
 
     files: [
       'lib/knockout/knockout.js',
-      'lib/requirejs/require.js',
-      'node_modules/karma-requirejs/lib/adapter.js',
+      // 'lib/requirejs/require.js',
+      // 'node_modules/karma-requirejs/lib/adapter.js',
 
-      'node_modules/karma-require.js',
+      // 'node_modules/karma-require.js',
 
-      'lib/angular/angular.js',
-      'lib/angular-animate/angular-animate.js',
-      'lib/angular-resource/angular-resource.js',
-      'lib/angular-route/angular-route.js',
-      '../node_modules/angular-mocks/angular-mocks.js',
+      // 'lib/angular/angular.js',
+      // 'lib/angular-animate/angular-animate.js',
+      // 'lib/angular-resource/angular-resource.js',
+      // 'lib/angular-route/angular-route.js',
+      // '../node_modules/angular-mocks/angular-mocks.js',
       
-      '**/*.module.js',
-      '*!(.module|.spec).js',
-      '!(lib)/**/*!(.module|.spec).js',
-      '**/*.spec.js',
-      '**/*[Ss]pec.js'
+      // '**/*.module.js',
+      // '*!(.module|.spec).js',
+      // '!(lib)/**/*!(.module|.spec).js',
+      // '**/*.spec.js',
+      // '**/*[Ss]pec.js',
+      'test/appSpec.js'
     ],
 
     autoWatch: true,
