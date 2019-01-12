c9188ddf8ed556f84e803483ee8b1e5c9f71ad28
David Shams <david.shams@gmail.com>
2019-01-12 13:10:09 -0600

    open webpack-dev-server to a specific page

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,29 @@ module.exports = {
         path: path.resolve(__dirname, "./build/"),
         filename: "bundle.js"
     },
-    mode: "development"
+    mode: "development",
+
+    devtool: 'inline-source-map',
+
+
+    devServer: {
+        contentBase: './',
+        host: 'localhost',
+        port: '9090',
+        inline: true,
+        compress: true,
+        proxy: {
+          '/api/**': {
+            target: 'http://10.189.1.159:8080',
+            secure: false,
+            changeOrigin: true,
+            cookieDomainRewrite: true
+          }
+        },
+
+        open: true, // Here
+        openPage: '/webpack.html' // And here
+      },
+    
 
 }
